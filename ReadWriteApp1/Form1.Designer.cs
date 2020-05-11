30fa8d60d6d04e0b155b9cecde885736d9a0f7ad
mr-subroutine <darrylshack@outlook.com>
2020-05-10 17:27:09 -0700

    added buttons for reading and writing and clearing the text

--- ReadWriteApp1/Form1.Designer.cs
+++ ReadWriteApp1/Form1.Designer.cs
@@ -31,6 +31,8 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -38,6 +40,7 @@
             this.textBox1.Location = new System.Drawing.Point(286, 38);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.textBox1.Size = new System.Drawing.Size(252, 247);
             this.textBox1.TabIndex = 0;
             // 
@@ -47,28 +50,53 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(153, 48);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Read Text";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(50, 106);
+            this.button2.Location = new System.Drawing.Point(50, 104);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(153, 48);
             this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
+            this.button2.Text = "Add (Write) Text";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(50, 170);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(153, 48);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(50, 237);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(153, 48);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Exit";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 328);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ReadWriteApp1 v1.0";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -79,6 +107,8 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- ReadWriteApp1/Form1.cs
+++ ReadWriteApp1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace ReadWriteApp1
 {
@@ -15,6 +16,43 @@ namespace ReadWriteApp1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string StartUpPath = Application.StartupPath;
+            string f = StartUpPath + @"\ff7.txt";
+
+            string[] lines = System.IO.File.ReadAllLines(@f);
+            foreach (string line in lines)
+            {
+                textBox1.Text += line;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string StartUpPath = Application.StartupPath;
+            string f = StartUpPath + @"\ff7.txt";
+            using (System.IO.StreamWriter file =
+            new System.IO.StreamWriter(@f, true))
+            {
+                file.WriteLine("This is added text.");
+            }
+            textBox1.Clear();
+            button1_Click(sender, e);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
